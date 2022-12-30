b56fce710669b5539c4165e9c1ee5004ffae484c
Philipp Ucke <64527676+pucke-uninow@users.noreply.github.com>
2022-12-30 14:34:48 +0100

    fix: update regex to support dashes
    Signed-off-by: Philipp Ucke <64527676+pucke-uninow@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import yaml from 'js-yaml'
 const httpClient = new HttpClient()
 
 export const IMAGE_REGEX =
-  /ghcr\.io\/(?<org>\w+)\/?(?<repo>\w+)?\/(?<imageName>\w+):(?<tag>.*)/
+  /ghcr\.io\/(?<org>[\w\-\.]*)\/?(?<repo>.*)?\/(?<imageName>.+):(?<tag>.*)/
 
 export function buildRegistryQueryUrl(
   org: string,
